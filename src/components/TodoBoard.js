f8626aa91f63c24247a7cb4a0e9393fb9f3aa46f
KILL-BURGER <dzaws@naver.com>
2024-10-19 05:42:34 +0900

    fix

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -6,15 +6,16 @@ const TodoBoard = ({todoList, updateComplete, deleteTask, index}) => {
     return (
         <div>
             <h2>Todo List</h2>
-            <div className='box'>
-                {todoList.length > 0
-                    ? todoList.map((item) => <TodoItem item={item}
-                                                       key={index}
-                                                       updateComplete={updateComplete}
-                                                       deleteTask={deleteTask}/>)
-                    : <Spinner />}
-            </div>
-
+            {todoList.length > 0
+                ? todoList.map((item) => <TodoItem item={item}
+                                                   key={index}
+                                                   updateComplete={updateComplete}
+                                                   deleteTask={deleteTask}/>)
+                :
+                <div className='box'>
+                    <Spinner/>
+                </div>
+            }
         </div>
     );
 };
